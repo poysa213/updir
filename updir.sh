f3bc5a33c8112f11585f52eb81fe7b4474cc6c27
poysa213 <hanaiayoucef@gmail.com>
2024-07-13 12:11:32 +0100

    upload the script

---
+++ updir.sh
@@ -0,0 +1,23 @@
+
+function updir() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: zdir <steps>"
+    echo "Example: zdir 1"
+    return 1
+  fi
+
+  local depth="$1"
+  local current_dir="$PWD"
+
+  if [[ -z "$current_dir" ]]; then
+    echo "Current directory not set"
+    return 1
+  fi
+
+  for (( i = 0; i < depth; i++ )); do
+    current_dir=$(dirname "$current_dir")
+  done
+
+  cd "$current_dir" || return 1
+}
+
